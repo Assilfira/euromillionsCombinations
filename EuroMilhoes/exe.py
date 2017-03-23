4ac7338798131837eff5513dbd0a942b43d65a7d
Nuno Duarte <neo_matrix_2@hotmail.com>
2017-03-23 12:57:55 +0000

    Add files via upload

---
+++ EuroMilhoes/exe.py
@@ -0,0 +1,64 @@
+import os, sys, time
+from random import randint
+
+# all combinations
+allCombinations = []
+
+def generate_key():
+    while True:
+        i = 1
+        iStars = 1
+        numbers = []
+        stars = []
+
+        # 5 numbers
+        for i in range(1, 6):
+            while True:
+                value = randint(1, 50)
+                if not value in numbers:
+                    numbers.append(value)
+                    break
+
+        # 2 stars
+        for iStars in range(1, 3):
+            while True:
+                star = randint(1, 12)
+                if not star in stars:
+                    stars.append(star)
+                    break
+
+        # order list
+        numbers.sort()
+        stars.sort()
+
+        combination = ' '.join(str(n) for n in numbers) + ' + ' + ' '.join(str(s) for s in stars)
+
+        # check if combination is unique
+        if not combination in allCombinations:
+            allCombinations.append(combination)
+            break
+
+
+    return combination
+
+
+while True:
+    nb = input("Number of combinations to generate: ")
+    try:
+        number = int(nb)
+        if (number < 0):
+            print("Error. Must be a positive number")
+            continue
+        elif (number >= 116531800):
+            print("Error. It is not possible to generate that number of combinations")
+            continue
+        break
+    except:
+        print("Error. Invalid input")
+
+
+
+
+# conitnue with the script and generate key
+for num in range(1,(number+1)):
+    print("("+str(num)+") "+generate_key())
